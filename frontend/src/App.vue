087e5f821d7e73218b84b2aa5a90136d07a6579e
SeaotterMS <peter910820@gmail.com>
2026-03-10 19:37:16 +0800

    feat(frontend): make new View and web struct

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,7 @@
 <template>
-  <router-view />
+  <v-app>
+    <router-view />
+  </v-app>
 </template>
 
 <script setup lang="ts"></script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  min-height: 100vh;
-  margin-bottom: 0px !important;
-}
-</style>
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,84 +1,31 @@
 <template>
-  <div class="main-style">
-    <div class="sticky-navbar">
-      <NavBar />
-    </div>
-    <div class="row">
-      <div class="col m12 m12 s12">
+  <div class="main-layout">
+    <NavBar />
+    <LoginModal v-model="showLoginModal" />
+    <v-main class="fs-main">
+      <v-container fluid class="pa-4">
         <router-view />
-      </div>
-    </div>
+      </v-container>
+    </v-main>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
 
 import NavBar from "@/components/NavBar.vue";
+import LoginModal from "@/components/LoginModal.vue";
+import { useLoginModal } from "@/store/loginModal";
 
-export default defineComponent({
-  components: {
-    NavBar,
-  },
-  setup() {
-    return;
-  },
-});
+const loginModalStore = useLoginModal();
+const { showLoginModal } = storeToRefs(loginModalStore);
 </script>
 
-<style>
-.main-style {
+<style scoped>
+.main-layout {
   min-height: 100vh;
-  min-width: 600px;
-  font-family: "madoufmg";
-
-  background-color: #f2ebea;
-  background-size: 400% 400%;
 }
-
-.row {
-  height: 100%;
-  margin-bottom: 0px !important;
-  padding: 30px;
-}
-.main-block {
-  padding: 40px;
-}
-.sub-block {
-  padding-top: 20px;
-  margin-top: 10px;
-  cursor: default !important;
-  border: 2px solid f2ebea;
-  border-radius: 20px;
-}
-.sticky-navbar {
-  position: sticky;
-  top: 0px;
-  z-index: 10;
-}
-.browser-default {
-  background-color: #f2ebea;
-  border: 0px;
-  border-bottom: 1px solid rgb(118, 118, 118);
-}
-/* hide materializecss select in mobile */
-.mobile-hidden {
-  display: block !important;
-}
-.mobile-display {
-  display: none !important;
-}
-@media (max-width: 768px) {
-  .mobile-display {
-    display: block !important;
-  }
-  .mobile-hidden {
-    display: none !important;
-  }
-}
-/* font-settings */
-@font-face {
-  font-family: "madoufmg";
-  src: url("@/assets/fonts/madoufmg.ttf") format("truetype");
+.fs-main {
+  background: rgb(var(--v-theme-background));
 }
 </style>
